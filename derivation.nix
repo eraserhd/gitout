4a48e8368fd4753bc388dbf7531d88ebacde6179
Jason Felice <jason.m.felice@gmail.com>
2020-04-06 10:03:51 -0400

    Add man pages

--- derivation.nix
+++ derivation.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchFromGitHub, gerbil, gerbilPackages, gambit, libyaml, zlib }:
+{ stdenv, fetchFromGitHub, gerbil, gerbilPackages, gambit, libyaml, zlib, asciidoc-full }:
 
 stdenv.mkDerivation rec {
   pname = "gitout";
@@ -6,11 +6,23 @@ stdenv.mkDerivation rec {
 
   src = ./.;
 
-  buildInputs = [ gerbil gambit gerbilPackages.clojerbil libyaml zlib ];
+  buildInputs = [
+    gerbil
+    gambit
+    gerbilPackages.clojerbil
+    libyaml
+    zlib
+    asciidoc-full
+  ];
 
   installPhase = ''
-    mkdir -p $out/bin
+    mkdir -p $out/bin $out/share/man/man1/
     GERBIL_PATH=$out GERBIL_LOADPATH=${gerbilPackages.clojerbil}/lib gxi build.ss
+
+    for mansource in *.1.adoc; do
+      a2x -f manpage "$mansource"
+      cp "''${mansource%.adoc}" $out/share/man/man1/
+    done
   '';
 
   meta = with stdenv.lib; {
